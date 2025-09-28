ffe9eddbdf2d46bb8754ab1cf3d0693b409c2a6c
xozi <apexcrow@gmail.com>
2025-09-28 12:33:56 -0800

    init

---
+++ target/debug/deps/modbot-7c05adbccbdd654b.d
@@ -0,0 +1,5 @@
+/Volumes/Backup 1TB/Dev/rustproj/modbot/target/debug/deps/modbot-7c05adbccbdd654b.d: src/main.rs
+
+/Volumes/Backup 1TB/Dev/rustproj/modbot/target/debug/deps/libmodbot-7c05adbccbdd654b.rmeta: src/main.rs
+
+src/main.rs:
